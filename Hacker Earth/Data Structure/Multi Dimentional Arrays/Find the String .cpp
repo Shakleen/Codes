2b35867e4bb54f0e3e6e2767d52cf9c5948b55d7
Ishrak <shakleenishfar@gmail.com>
2018-02-11 10:39:37 +0600

    11 Feb 2018

---
+++ Hacker Earth/Data Structure/Multi Dimentional Arrays/Find the String .cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//*******************************************************************************
+//*******************************Template****************************************
+//*******************************************************************************
+
+#define eps                 1e-6
+#define ll                  long long
+#define llu                 long long unsigned
+#define ld                  long double
+#define REP(i,n)            for(int i = 0; i < n; i++)
+#define FLR(i,n)            for(ll i = 0; i < n; i++)
+#define ROF(i,n)            for(int i = n-1; i >= 0; i--)
+#define FOR(i,a,b)          for(int i = a; i <= b; i++)
+#define FORL(i,a,b)         for(ll i = a; i <= b; i++)
+#define casep(z)            printf("Case %d:", z);
+#define sz(a)               a.size()
+#define all(x)              x.begin(),x.end()
+#define SORT(a,n)           sort(a,a+n)
+#define RSORT(a,n)          sort(a,a+n,greater<ll>())
+#define VSORT(v)            sort(all(v))
+#define VRSORT(v)           sort(all(v),greater<ll>())
+#define pii                 pair <int, int>
+#define pll                 pair <ll, ll>
+#define pb                  push_back
+#define pf                  push_front
+#define pob                 pop_back()
+#define pof                 pop_front()
+#define ff                  first
+#define ss                  second
+#define mp                  make_pair
+#define ms(x,n)             memset((x),n,sizeof(x))
+#define fin                 freopen("in.txt", "r", stdin)
+#define fout                freopen("out.txt", "w", stdout)
+#define IT(it,x)            for(it=x.begin(); it != x.end(); it++)
+#define sf1(x)              scanf("%d", &x)
+#define sfl1(x)             scanf("%lld", &x)
+
+//int fx[] = {1,-1,0,0};
+//int fy[] = {0,0,1,-1};
+
+void FAST(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int main(){
+    FAST();
+
+//    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+
+//    clock_t begin, end;
+//    double time_spent;
+//    begin = clock();
+
+    int t=0,n=0,m=0,temp=0, idx=0;
+    string matrix[500], str;
+    bool flag = false;
+
+    cin>>t;
+
+    FOR(tc,1,t){
+        n = m = flag = temp = 0;
+
+        cin>>n>>m;
+
+        REP(i,n)    cin>>matrix[i];
+
+        cin>>str;
+
+        REP(i,str.size()){
+            temp = i%n, idx = matrix[temp].find(str[i]);
+
+            if(idx >= m || idx<0){
+                flag = true;
+                break;
+            }
+            else{
+                matrix[temp][idx] = '(';
+            }
+//            cout<<str[i]<<' '<<temp<<' '<<idx<<' '<<flag<<endl;
+        }
+
+        if(flag)    cout<<"No\n";
+        else        cout<<"Yes\n";
+
+//        REP(i,n)    matrix[i].clear();
+//        str.clear();
+    }
+
+
+//    end = clock();
+//    time_spent = (double)(end-begin)/CLOCKS_PER_SEC;
+//    printf("TIme spent = %lf", time_spent);
+
+    return 0;
+}
+/*
+2
+3 3
+adg
+xfh
+cei
+axcde
+3 3
+aba
+aba
+acd
+aaaaa
+*/
+
